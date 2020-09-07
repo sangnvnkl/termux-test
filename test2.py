f104a52a2311510f6c4a61bc4f541d51607bb976
SangNguyen <sangnvnkl@gmail.com>
2020-09-07 16:47:28 +0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,50 @@
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+def keluar():
+	print "\x1b[1;91mExit"
+	os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+    
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.07)
+try:
+  br.open('https://m.facebook.com')
+except mechanize.URLError:
+  print"\n\x1b[1;96mThere is no internet connection"
+  keluar()
+br._factory.is_html = True
+br.select_form(nr=0)
+br.form['email'] = 'sangnv'
+br.form['pass'] = 'passofsangnv'
+br.submit()
+url = br.geturl()
